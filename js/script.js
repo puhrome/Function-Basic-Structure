f7697bb9e05a564bd1cdc5637da2523deded841e
puhrome <phirom.yim@fullsail.edu>
2014-04-29 17:52:01 -0400

    Basic Structure of a Function
    Learning how to write the basic structure of a function in WebStorm

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/**
+ * Basic Structure Created by Phirom Yim on 4/29/14.
+ */
+//function only written once
+function outptMsg() {
+    console.log("Hello World");//inside curly braces, block of code
+}
+
+function calcArea(){
+    var width = 20;//code inside braces
+    var height = 30;//variable for height
+    var area = width * height;//variable for are
+    console.log(area);//print code
+}
